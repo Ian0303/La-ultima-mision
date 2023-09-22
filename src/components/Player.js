2693422f49dd67d92671e87fd8f90634307f87d7
Federico Degiovanni <federicoadegiovanni@gmail.com>
2023-09-22 19:39:03 -0300

    fix anims

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,44 +1,37 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
+export default class Player extends Phaser.GameObjects.Sprite {
+  velocity;
 
-export default class Player extends Phaser.GameObjects.Sprite
-{
-velocity;
+  body;
 
-body;
+  cursors;
 
-cursors
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture);
 
-constructor(scene, x, y, texture)
-{
-super(scene, x, y, texture);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
 
-scene.add.existing(this);
-scene.physics.add.existing(this);
+    this.body.setCollideWorldBounds(true);
 
-this.body.setCollideWorldBounds(true);
+    this.setTexture(texture);
+    this.velocity = 500;
 
-this.setTexture(texture)
-this.velocity = 500;
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
 
+  update() {
+    if (this.cursors.left.isDown) {
+      console.log("izquierda");
+      this.body.setVelocityX(-this.velocity);
+      this.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.body.setVelocityX(this.velocity);
+      this.anims.play("right", true);
+    } else {
+      this.body.setVelocityX(0);
+      this.anims.play("turn");
+    }
+  }
 }
-
-
-update(){
-	if (this.cursors.left.isDown) {
-		this.body.velocityx(-this.velocity);
-		this.body.anims.play("left", true);
-	  }
-	  else if (this.cursors.right.isDown) {
-		this.body.velocity(this.velocity);
-		this.body.anims.play("right", true);
-	  }
-	  else {
-		this.body.velocity(0);
-		this.body.anims.play("turn");
-	  }   
-}
-
-}
-
-
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,35 +2,31 @@ import Phaser from "phaser";
 import Player from "../components/Player";
 
 export default class Game extends Phaser.Scene {
-
-
   constructor() {
     super("game");
     this.player = null;
-
   }
 
   init() {
-    this.cursors = this.input.keyboard.createCursorKeys()
-
-
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-
   create() {
-
-    this.add.image(320, 450, "openCameras").setDepth(1)
+    this.add
+      .image(320, 450, "openCameras")
+      .setDepth(1)
       .setScale(0.7)
-      .setInteractive().on("pointerdown", () => this.scene.launch("cameras"));
+      .setInteractive()
+      .on("pointerdown", () => this.scene.launch("cameras"));
     this.add.image(320, 240, "room");
-    console.log("si")
+    console.log("si");
 
     this.player = new Player(this, 300, 280, "player");
 
-    this.cameras.main.startFollow(this.player)
-      .setFollowOffset(0, 100);
-    
-
+    this.cameras.main.startFollow(this.player).setFollowOffset(0, 100);
   }
 
+  update() {
+    this.player.update();
+  }
 }
