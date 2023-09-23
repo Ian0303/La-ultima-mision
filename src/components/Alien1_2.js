000c9bf09fc2aefa4a3c994e1d2a1ab59f080517
Ian0303 <ian.gonza03@gmail.com>
2023-09-22 21:21:31 -0300

    alien1 v1

---
+++ src/components/Alien1_2.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser'
+
+export default class Player extends Phaser.GameObjects.Sprite
+{
+room;
+
+
+constructor(scene, x, y, texture, activo){
+super(scene, x, y, texture, activo);
+    this.newRoom = undefined;
+    this.active = true
+}
+
+
+
+
+
+
+/* moverAlien(room) {
+    if (this.active === true) {
+        this.newRooom = Phaser.Math.Between(1, 5)
+        this.room = this.newRoom 
+        if (this.room === 1) {
+            
+        }
+
+    }
+} */
+    
+}
